fe6f4d9c0b46df78283ec3c15b9be50912235158
T.S. Foster <hi@tsf.io>
2017-01-31 12:01:51 +0000

    Install elm-test

---
+++ tests/Tests.elm
@@ -0,0 +1,11 @@
+module Tests exposing (..)
+
+import Test exposing (..)
+import Expect
+import Fuzz exposing (list, int, tuple, string)
+
+
+all : Test
+all =
+    describe "Test Suite"
+        []
